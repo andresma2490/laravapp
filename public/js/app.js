4491ef77be3fbb154cbbde7060fc2fc6c611d45f
andresma2490 <montenegro.andres@correounivalle.edu.co>
2020-03-14 09:03:23 -0500

    vue

--- public/js/app.js
+++ public/js/app.js
@@ -37318,9 +37318,7 @@ var staticRenderFns = [
             ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
+              _vm._v("\n                    ji ji ji \n                ")
             ])
           ])
         ])
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
-@extends('appTemplate')
+@extends('layouts.app')
 
 @section('title')
     Bienvenid@
+@endsection
+
+@section('content')
+    <example-component/>
 @endsection
